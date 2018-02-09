2876fce3e27fafcbbfeb86dc173a8f631004aa1d
Richard Wardin <shalmezad@gmail.com>
2018-02-08 19:57:44 -0500

    Draw 'robot'

--- main.py
+++ main.py
@@ -7,20 +7,46 @@ SCREEN_Y = 600
 STARTING_Y = 435
 PIXELS_PER_FOOT = 13.75
 DEGREES_PER_RADIAN = 57.2958
+
+# Colors 
 YELLOW = (255, 255, 0)
 GREEN =(0 ,204 ,0)
+
+# Button positions
 BTN_LL = pygame.Rect((0, 479, 105, 50))
 BTN_LR = pygame.Rect((105, 479, 105, 50))
 BTN_RL = pygame.Rect((210, 479, 105, 50))
 BTN_RR = pygame.Rect((315, 479, 105, 50))
 BTN_EXPORT = pygame.Rect((0, 529, 105, 50))
 
+# Robot dimensions
+ROBOT_DIMS_INCHES=[32.5, 27.5]
+ROBOT_DIMS_FEET=[inches/12 for dim in ROBOT_DIMS_INCHES]
+
+def drawRobot(screen, point, angle, color):
+    baseAngle = 45 + angle
+    angles = [baseAngle, baseAngle + 90, baseAngle + 180, baseAngle + 270]
+    rads = [math.radians(angle) for angle in angles]
+    x_points = [math.cos(rad)*20+point[0] for rad in rads]
+    y_points = [math.sin(rad)*20+point[1] for rad in rads]
+    points = []
+    for i in range(len(x_points)):
+        points.append([x_points[i], y_points[i]])
+    pygame.draw.polygon(screen, color, points, 1)
+
+
+
 def drawPath(screen, path):
         for point in path:
             pygame.draw.circle(screen, YELLOW, point, 2, 0)
         
         for x in range(len(path) - 1):
-            pygame.draw.line(screen, YELLOW, path[x], path[x + 1], 1)
+            last_point = path[x]
+            next_point = path[x+1]
+            angle = calcAngle(last_point, next_point)
+            drawRobot(screen, last_point, angle, 0xFFDDAAAA)
+            drawRobot(screen, next_point, angle, 0xFFAADDAA)
+            pygame.draw.line(screen, YELLOW, last_point, next_point, 1)
 
 
 def main():
