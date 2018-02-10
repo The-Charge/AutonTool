d7a7f7a831472ac73136949a61fafd5821bef5ec
Yand1 <yaandofficial@gmail.com>
2018-02-10 08:53:51 -0500

    Fixed robot drawings

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import math
 
 SCREEN_X = 421
 SCREEN_Y = 600
-#Start represents front of robot 
+#Start represents center of robot 
 STARTING_Y = 435
 PIXELS_PER_FOOT = 13.75
-DEGREES_PER_RADIAN = 57.2958
 
 # Colors 
 YELLOW = (255, 255, 0)
-GREEN =(0 ,204 ,0)
+GREEN = (0 ,204 ,0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 # Button positions
 BTN_LL = pygame.Rect((0, 479, 105, 50))
@@ -20,15 +21,17 @@ BTN_RR = pygame.Rect((315, 479, 105, 50))
 BTN_EXPORT = pygame.Rect((0, 529, 105, 50))
 
 # Robot dimensions
-ROBOT_DIMS_INCHES=[32.5, 27.5]
-ROBOT_DIMS_FEET=[inches/12 for dim in ROBOT_DIMS_INCHES]
+ROBOT_DIMS_INCHES = [38.5, 33.5]
+ROBOT_DIMS_FEET = [dim / 12 for dim in ROBOT_DIMS_INCHES]
+ROBOT_DIAG_FEET = math.sqrt(ROBOT_DIMS_FEET[0] ** 2 + ROBOT_DIMS_FEET[1] ** 2) / 2
 
 def drawRobot(screen, point, angle, color):
-    baseAngle = 45 + angle
-    angles = [baseAngle, baseAngle + 90, baseAngle + 180, baseAngle + 270]
+    baseAngle = 48.972495940751 + angle
+    baseAngle -= 90
+    angles = [baseAngle, baseAngle + 82.055008118498, baseAngle + 180, baseAngle + 262.0550081185]
     rads = [math.radians(angle) for angle in angles]
-    x_points = [math.cos(rad)*20+point[0] for rad in rads]
-    y_points = [math.sin(rad)*20+point[1] for rad in rads]
+    x_points = [math.cos(rad) * ROBOT_DIAG_FEET * PIXELS_PER_FOOT + point[0] for rad in rads]
+    y_points = [math.sin(rad) * ROBOT_DIAG_FEET * PIXELS_PER_FOOT + point[1] for rad in rads]
     points = []
     for i in range(len(x_points)):
         points.append([x_points[i], y_points[i]])
@@ -37,16 +40,18 @@ def drawRobot(screen, point, angle, color):
 
 
 def drawPath(screen, path):
-        for point in path:
-            pygame.draw.circle(screen, YELLOW, point, 2, 0)
-        
-        for x in range(len(path) - 1):
-            last_point = path[x]
-            next_point = path[x+1]
-            angle = calcAngle(last_point, next_point)
-            drawRobot(screen, last_point, angle, 0xFFDDAAAA)
-            drawRobot(screen, next_point, angle, 0xFFAADDAA)
-            pygame.draw.line(screen, YELLOW, last_point, next_point, 1)
+    for point in path:
+        pygame.draw.circle(screen, YELLOW, point, 2, 0)
+    
+    for x in range(len(path) - 1):
+        last_point = path[x]
+        next_point = path[x + 1]
+        angle = calcAngle(last_point, next_point)
+        drawRobot(screen, last_point, angle, BLUE)
+        drawRobot(screen, next_point, angle, RED)
+        pygame.draw.line(screen, YELLOW, last_point, next_point, 1)
+    if len(path) > 0:
+        drawRobot(screen, path[0], 0, BLUE)
 
 
 def main():
@@ -72,6 +77,7 @@ def main():
         pygame.draw.rect(screen, GREEN, BTN_RL, 0) 
         pygame.draw.rect(screen, YELLOW, BTN_RR, 0) 
         pygame.draw.rect(screen, YELLOW, BTN_EXPORT, 0)
+        
         
         drawPath(screen, paths[currentPath])
         
@@ -108,6 +114,10 @@ def main():
                     elif indexClicked == 3:
                         currentPath = "RR"
                     elif indexClicked == 4:
+                        outputListLL = []
+                        outputListLR = []
+                        outputListRL = []
+                        outputListRR = []
                         outputLL = ""
                         outputLR = ""
                         outputRL = ""
@@ -117,17 +127,21 @@ def main():
                             p2 = paths["LL"][x + 1]
                             
                             angle = calcAngle(p1, p2)
-                            outputLL += str(round(angle, 2)) + "deg"
+                            if angle != 0:
+                                outputListLL.append((str(round(angle, 2)), "deg"))
                             
                             distance = calcDist(p2, p1) / PIXELS_PER_FOOT
-                            outputLL += str(round(distance, 2)) + "ft"
-                            
+                            outputListLL.append((str(round(distance, 2)), "ft"))
                         for x in range(len(paths["LR"]) - 1):
                             pass
                         for x in range(len(paths["RL"]) - 1):
                             pass
                         for x in range(len(paths["RR"]) - 1):
                             pass
+                        
+                        for output in outputListLL:
+                            outputLL += output[0] + output[1]
+                            
                         print("LL: " + outputLL)
                    
                     pygame.display.set_caption("AutonTool: " + currentPath)
@@ -142,7 +156,7 @@ def main():
 
 #Returns the distance between two pygame positions
 def calcDist(p1, p2):
-    return math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+    return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 #Returns absolute angle from -180 to 180 between two pygame positions, with zero being straight forward
 def calcAngle(p1, p2):
@@ -152,21 +166,20 @@ def calcAngle(p1, p2):
     delta_y = point2[1] - point1[1]
     
     if delta_x == 0 and delta_y > 0:
-        theta_degrees = 90
+        theta = 90
     elif delta_x == 0 and delta_y < 0:
-        theta_degrees = -90
+        theta = -90
     else:
-        theta_radians = math.atan2(delta_y, delta_x)
-        theta_degrees = theta_radians * DEGREES_PER_RADIAN
+        theta = math.degrees(math.atan2(delta_y, delta_x))
         
-    theta_degrees -= 90
-    if theta_degrees < -180:
-        theta_degrees += 360
+    theta -= 90
+    if theta < -180:
+        theta += 360
     
-    if theta_degrees != 180 and theta_degrees != 0:
-        theta_degrees *= -1
+    if theta != 180 and theta != 0:
+        theta *= -1
         
-    return theta_degrees
+    return theta
     
 if __name__=="__main__":
     main()
